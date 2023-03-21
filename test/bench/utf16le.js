59cbb6795aa47c51b8907a136443afcc759bd7dd
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-21 12:41:35 +0100

    Add `utf16le` test and benchmark

---
+++ test/bench/utf16le.js
@@ -0,0 +1,26 @@
+const test = require('brittle')
+const Buffer = require('../..')
+
+test('Buffer.from(string, \'utf16le\')', async (t) => {
+  const string = Buffer.alloc(4 * 1024, 'a').toString('utf16le')
+
+  const elapsed = await t.execution(() => {
+    for (let i = 0; i < 1e5; i++) {
+      Buffer.from(string, 'utf16le')
+    }
+  })
+
+  t.comment(1e5 / elapsed * 1e3 | 0, 'ops/s')
+})
+
+test('buffer.toString(\'utf16le\')', async (t) => {
+  const buffer = Buffer.alloc(4 * 1024, 'a')
+
+  const elapsed = await t.execution(() => {
+    for (let i = 0; i < 1e5; i++) {
+      buffer.toString('utf16le')
+    }
+  })
+
+  t.comment(1e5 / elapsed * 1e3 | 0, 'ops/s')
+})
---
+++ test/utf16le.js
@@ -0,0 +1,18 @@
+const test = require('brittle')
+const Buffer = require('..')
+
+test('utf16le byteLength', (t) => {
+  t.is(Buffer.byteLength('hello world', 'utf16le'), 22)
+})
+
+test('utf16le toString', (t) => {
+  t.is(Buffer.from([0x68, 0x00, 0x65, 0x00, 0x6c, 0x00, 0x6c, 0x00, 0x6f, 0x00, 0x20, 0x00, 0x77, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6c, 0x00, 0x64, 00]).toString('utf16le'), 'hello world')
+})
+
+test('utf16le write', (t) => {
+  const buffer = Buffer.alloc(Buffer.byteLength('hello world', 'utf16le'))
+
+  t.is(buffer.write('hello world', 'utf16le'), 22)
+
+  t.alike(buffer, Buffer.from('hello world', 'utf16le'))
+})
