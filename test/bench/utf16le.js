2f9a0b685af63011ab179cea9c2d9c12f448b729
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-06 12:50:28 +0100

    Use typed function API

--- test/bench/utf16le.js
+++ test/bench/utf16le.js
@@ -1,26 +1,38 @@
 const test = require('brittle')
 const Buffer = require('../..')
 
+test("Buffer.byteLength(string, 'utf16le')", async (t) => {
+  const string = Buffer.alloc(4 * 1024, 'a').toString('utf16le')
+
+  const elapsed = await t.execution(() => {
+    for (let i = 0; i < 1e7; i++) {
+      Buffer.byteLength(string, 'utf16le')
+    }
+  })
+
+  t.comment(((1e7 / elapsed) * 1e3) | 0, 'ops/s')
+})
+
 test("Buffer.from(string, 'utf16le')", async (t) => {
   const string = Buffer.alloc(4 * 1024, 'a').toString('utf16le')
 
   const elapsed = await t.execution(() => {
-    for (let i = 0; i < 1e5; i++) {
+    for (let i = 0; i < 1e6; i++) {
       Buffer.from(string, 'utf16le')
     }
   })
 
-  t.comment(((1e5 / elapsed) * 1e3) | 0, 'ops/s')
+  t.comment(((1e6 / elapsed) * 1e3) | 0, 'ops/s')
 })
 
 test("buffer.toString('utf16le')", async (t) => {
   const buffer = Buffer.alloc(4 * 1024, 'a')
 
   const elapsed = await t.execution(() => {
-    for (let i = 0; i < 1e5; i++) {
+    for (let i = 0; i < 1e6; i++) {
       buffer.toString('utf16le')
     }
   })
 
-  t.comment(((1e5 / elapsed) * 1e3) | 0, 'ops/s')
+  t.comment(((1e6 / elapsed) * 1e3) | 0, 'ops/s')
 })
--- test/bench/utf8.js
+++ test/bench/utf8.js
@@ -1,6 +1,18 @@
 const test = require('brittle')
 const Buffer = require('../..')
 
+test("Buffer.byteLength(string, 'utf8')", async (t) => {
+  const string = Buffer.alloc(4 * 1024, 'a').toString('utf8')
+
+  const elapsed = await t.execution(() => {
+    for (let i = 0; i < 1e6; i++) {
+      Buffer.byteLength(string, 'utf8')
+    }
+  })
+
+  t.comment(((1e6 / elapsed) * 1e3) | 0, 'ops/s')
+})
+
 test("Buffer.from(string, 'utf8')", async (t) => {
   const string = Buffer.alloc(4 * 1024, 'a').toString('utf8')
 
